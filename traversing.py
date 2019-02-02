5369302c099b1f3c89413c1579b768f3243b41d8
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-01 16:54:07 -0800

    printing the content of all the directories in home directory and all files

--- traversing.py
+++ traversing.py
@@ -3,31 +3,21 @@
 #    printing current working directory
 
 import os
-cwd = os.getcwd()
-print("Current working directory: ",cwd,end="\n\n\n")
 
-#    printing whole content(all files and folders listed) of home directory
+path_to_home ='/home/ankit'
 
 print ("Content of Home directory:",end="\n\n\n")
 
-for item in os.listdir('/home/ankit'):
-    print(item)
 
-#    printing only files of home directory
+def directory_content(directory):
 
-print ("Files only of Home directory:",end="\n\n\n")
+    for item in os.listdir(directory):
+        
+        path = os.path.join(directory,item)
+        if os.path.isfile(path):
+            print(path)
 
-for item in os.listdir('/home/ankit'):
+        else:
+            directory_content(path)
 
-    if os.path.isfile(item):
-        print(item)
-
-#    printing only Folders of home directory
-
-print ("Folders only of Home directory:",end="\n\n\n")
-
-for item in os.listdir('/home/ankit'):
-
-    if os.path.isdir(item):
-        print(item)
-
+directory_content(path_to_home)
