1c897e80be069a4bedf5c24bee7b5a615afff932
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-01 16:32:00 -0800

    printing files and folders separately as well as together in a single content

--- traversing.py
+++ traversing.py
@@ -1,10 +1,33 @@
 # This is a program for traversing content of home folder as a part of classroom task1
 
+#    printing current working directory
 
 import os
 cwd = os.getcwd()
-print(cwd)
+print("Current working directory: ",cwd,end="\n\n\n")
 
-for item in os.listdir(cwd):
-    print(item)	
+#    printing whole content(all files and folders listed) of home directory
+
+print ("Content of Home directory:",end="\n\n\n")
+
+for item in os.listdir('/home/ankit'):
+    print(item)
+
+#    printing only files of home directory
+
+print ("Files only of Home directory:",end="\n\n\n")
+
+for item in os.listdir('/home/ankit'):
+
+    if os.path.isfile(item):
+        print(item)
+
+#    printing only Folders of home directory
+
+print ("Folders only of Home directory:",end="\n\n\n")
+
+for item in os.listdir('/home/ankit'):
+
+    if os.path.isdir(item):
+        print(item)
 
